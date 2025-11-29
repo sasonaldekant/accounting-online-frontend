84cec30212cd94c1c4c8ddddbcc5203fac7a0365
Sasonal Dekant <sasonal.dekant@gmail.com>
2025-11-29 01:05:09 +0100

    feat: Add pages index export and update README with implementation status

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { DashboardPage } from './DashboardPage';
+export { DocumentListPage } from './DocumentListPage';
+export { DocumentCreatePage } from './DocumentCreatePage';
+export { DocumentDetailPage } from './DocumentDetailPage';
